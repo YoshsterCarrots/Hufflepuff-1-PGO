856a9ba7ca2ee9330771272acc57b9154c3ff94e
ryan_1024675 <1024675@my.scccd.edu>
2026-03-10 16:31:46 -0700

    Separated titles

--- main.cc
+++ main.cc
@@ -1,7 +1,35 @@
 #include "/public/read.h" // IWYU pragma: keep
 #include <vector>         // IWYU pragma: keep
+#include <iostream>       // IWYU pragma: keep
+#include <fstream>        // IWYU pragma: keep
 using namespace std;
 
+
 int main() {
-	cout << "Hello World!" << endl;
+	ifstream videos("USvideos.csv");
+	ofstream outputFile("titles.txt");
+	string currLine;
+
+	getline(videos, currLine);
+	while (getline(videos, currLine)) {
+		int commaCount = 0;
+		bool isInQuote = false;
+		for (char c : currLine) {
+			if (commaCount > 2) {
+				break;
+			}
+			else if (c == '"') {
+				isInQuote = !isInQuote;
+			}
+			else if (!isInQuote && c == ',') {
+				commaCount++;
+			}
+			else if (commaCount == 2) {
+				outputFile << c;
+			}
+		}
+		outputFile << '\n';
+	}
+
+	outputFile.close();
 }
