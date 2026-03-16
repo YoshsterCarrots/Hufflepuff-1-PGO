91a47e817c77979ec36bc341e30abf8f53c51f70
ryan_1024675 <1024675@my.scccd.edu>
2026-03-15 20:07:01 -0700

    Updated parsing

--- main.cc
+++ main.cc
@@ -5,10 +5,13 @@
 #include <unordered_map>
 using namespace std;
 
+void parseVideos(ifstream& videos, ofstream& outputFile);
 
 int main() {
 	ifstream videos("USvideos.csv");
 	ofstream outputFile("compiledVideos.txt");
+	
+	parseVideos(videos, outputFile);
 
 	outputFile.close();
 }
@@ -32,8 +35,12 @@ void parseVideos(ifstream& videos, ofstream& outputFile) {
 	while (getline(videos, currLine)) {
 		int commaCount = 0;
 		bool isInQuote = false;
+		bool isAvailable = true;
 		for (char c : currLine) {
-			if (commaCount > 2) {
+			if (commaCount == 14) {
+				if (c == 'T') {
+					isAvailable = false;
+				}
 				break;
 			}
 			else if (c == '"') {
@@ -42,10 +49,83 @@ void parseVideos(ifstream& videos, ofstream& outputFile) {
 			else if (!isInQuote && c == ',') {
 				commaCount++;
 			}
+		}
+		if (!isAvailable) {
+			continue;
+		}
+		commaCount = 0;
+		isInQuote = false;
+		string currID = "";
+		string currTrendDate = "";
+		string currTitle = "";
+		string currChannel = "";
+		string currPubTime = "";
+		vector<string> currTagList = {};
+		string currTag = "";
+		string currViews = "";
+		string currLikes = "";
+		string currDislikes = "";
+		string currCommCount = "";
+		for (char c : currLine) {
+			if (c == '"') {
+				isInQuote = !isInQuote;
+			}
+			else if (!isInQuote && c == ',') {
+				if (currTag != "") {
+					currTagList.push_back(currTag);
+					currTag = "";
+				}
+				commaCount++;
+				outputFile << ' ';
+			}
+			else if (commaCount == 0) {
+				outputFile << c;
+				currID += c;
+			}
+			else if (commaCount == 1) {
+				outputFile << c;
+				currTrendDate += c;
+			}
 			else if (commaCount == 2) {
 				outputFile << c;
+				currTitle += c;
+			}
+			else if (commaCount == 3) {
+				outputFile << c;
+				currChannel += c;
+			}
+			else if (commaCount == 5) {
+				outputFile << c;
+				currPubTime += c;
+			}
+			else if (commaCount == 6) {
+				outputFile << c;
+				if (!isInQuote && c == '|') {
+					currTagList.push_back(currTag);
+					currTag = "";
+				}
+				else {
+					currTag += c;
+				}
+			}
+			else if (commaCount == 7) {
+				outputFile << c;
+				currViews += c;
+			}
+			else if (commaCount == 8) {
+				outputFile << c;
+				currLikes += c;
+			}
+			else if (commaCount == 9) {
+				outputFile << c;
+				currDislikes += c;
+			}
+			else if (commaCount == 10) {
+				outputFile << c;
+				currCommCount += c;
 			}
 		}
+		//Video tempVideo(currTitle, currChannel, currTrendDate, currPubTime, currID, stoi(currViews), stoi(currLikes), stoi(currDislikes), stoi(currCommCount), 0);
 		outputFile << '\n';
 	}
 
