b25720859474bf49351ba428d3b09644a80e90d9
silicant <1014434@my.scccd.edu>
2026-03-17 19:43:59 -0700

    made the map hierarchy simpler

--- Compilation.h
+++ Compilation.h
@@ -13,14 +13,14 @@ enum Nationality {
 };	
 
 struct Compilation {
-	std::unordered_map<char, std::unordered_map<std::string, Video>> compilation = {};
+	std::unordered_map<std::string, Video> compilation = {};
 	std::unordered_map<std::string, int> tags = {};
 
 	enum Nationality compilationCountry = DEFAULT;
 
 	Compilation(enum Nationality newCountry);
 
-	void insertVideo(Video newVideo, char letter, const std::string &newVidTitle, const std::vector<std::string> &newTags);
+	void insertVideo(Video newVideo, const std::string &newVidTitle, const std::vector<std::string> &newTags);
 
 	void compile(std::unordered_map<std::string, Video> &videosFinal, std::unordered_map<std::string, int> &tagsFinal); 
 };
