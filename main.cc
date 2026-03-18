c465ff9f408c823dcac41319885efa27875116b8
ryan_1024675 <1024675@my.scccd.edu>
2026-03-17 21:18:39 -0700

    Tweak more

--- main.cc
+++ main.cc
@@ -202,7 +202,7 @@ void parseVideos(ifstream& videos, ofstream& outputFile, Compilation& comp) {
 		outputFile << '\n';
 		*/
 		currTrendDate = to_string(currTrendDate.at(0)) + currTrendDate.at(1) + currTrendDate.at(6) + currTrendDate.at(7) + currTrendDate.at(3) + currTrendDate.at(4);
-		Video tempVideo(currTitle, currChannel, stoi(currTrendDate), currPubTime, currID, stoi(currViews), stoi(currLikes), stoi(currDislikes), stoi(currCommCount));
+		Video tempVideo(currTitle, currChannel, currPubTime, currID, stoi(currTrendDate), stoi(currViews), stoi(currLikes), stoi(currDislikes), stoi(currCommCount));
 		comp.insertVideo(tempVideo, currTitle, currTagList);
 	}
 }
